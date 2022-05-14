93921df6083fd3633b0f70cc0b3d2b72bccdda12
stao <3288182@qq.com>
2022-05-15 01:19:15 +0800

    v0.9.7

--- bean/bean.go
+++ bean/bean.go
@@ -1,210 +1,115 @@
 package bean
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 	"reflect"
 	"unsafe"
 )
 
-// Copy 将数据源对象复制到目标对象
+// Copy 将数据源复制到目标对象
 // @source 数据源对象
-// @target 目标对象
-func Copy(source, target interface{}) {
-	//获取reflect.Type类型
-	//sourceType := reflect.TypeOf(source)
-	//sourceType := reflect.TypeOf(source)
-	sourceValue := reflect.ValueOf(source).Elem()
-	targetValue := reflect.ValueOf(target).Elem()
-	sourceType := sourceValue.Type()
-	for i := 0; i < sourceValue.NumField(); i++ {
-		// 在要修改的结构体中查询有数据结构体中相同属性的字段，有则修改其值
-		name := sourceType.Field(i).Name
-		if ok := targetValue.FieldByName(name).IsValid(); ok {
-			targetValue.FieldByName(name).Set(reflect.ValueOf(sourceValue.Field(i).Interface()))
-		}
-	}
-}
-
-func Copy2(source, dest interface{}) error {
-
-	sourceType := reflect.TypeOf(source)
+// @dest 目标对象
+func Copy(source, dest interface{}) error {
 	sourceValue := reflect.ValueOf(source)
-	targetType := reflect.TypeOf(dest)
-	targetValue := reflect.ValueOf(dest)
-
-	fmt.Println(sourceValue.CanSet())
-	//fmt.Println(targetValue.CanSet())
-
-	fmt.Println("sourceType:", sourceType)   // []*bean.FruitA
-	fmt.Println("sourceValue:", sourceValue) // [0xc000058740 0xc000058780 ... 0xc000058980 0xc0000589c0]
-	fmt.Println("targetType:", targetType)   // []*bean.FruitB
-	fmt.Println("targetValue:", targetValue) // []
+	//targetType := reflect.TypeOf(dest)
+	//if !destValue.CanSet() {
+	//	return errors.New("dest value can't be set")
+	//}
+	//fmt.Println("sourceType:", sourceType)   // []*bean.FruitA
+	//fmt.Println("sourceValue:", sourceValue) // [0xc000058740 0xc000058780 ... 0xc000058980 0xc0000589c0]
+	//fmt.Println("targetType:", targetType)   // []*bean.FruitB
+	//fmt.Println("destValue:", destValue) // []
 	//fmt.Println("TypeType:", reflect.TypeOf(targetType)) // *reflect.rtype
-	switch sourceType.Kind() {
+	switch sourceValue.Type().Kind() {
 	case reflect.Array, reflect.Slice:
-		//targetValue := reflect.ValueOf(dest).Elem()
-		//if targetValue.Type().Kind() != reflect.Ptr {
-		//	return errors.New("接收不是指针类型")
-		//}
-
-		//targetValue := reflect.ValueOf(dest)
-		fmt.Println("targetValue:", targetValue) // &[]
-		for targetValue.Kind() == reflect.Ptr {
-			if targetValue.IsNil() && targetValue.CanAddr() {
-				targetValue.Set(reflect.New(targetValue.Type().Elem()))
-			}
-			targetValue = targetValue.Elem()
+		destValue := reflect.ValueOf(dest)
+		if destValue.Kind() != reflect.Ptr {
+			return errors.New("dest value can't a pointer type")
 		}
-		fmt.Println("targetValue:", targetValue) // []
-		if !targetValue.IsValid() {
-			return errors.New("接收不是指针类型")
-			// db.AddError(ErrInvalidValue)
+		for sourceValue.Kind() == reflect.Ptr {
+			sourceValue = sourceValue.Elem()
 		}
-
+		if !sourceValue.IsValid() {
+			return errors.New("source value invalid")
+		}
+		for destValue.Kind() == reflect.Ptr {
+			destValue = destValue.Elem()
+		}
+		if !destValue.IsValid() {
+			return errors.New("dest value invalid")
+		}
+		destItemType := destValue.Type().Elem()
+		fmt.Println(destItemType) // *bean.FruitB
+		ptrLevel := 0
+		for destItemType.Kind() == reflect.Ptr {
+			ptrLevel++
+			destItemType = destItemType.Elem()
+			fmt.Println(destItemType) // bean.FruitB
+		}
 		//targetTypeSlice := reflect.MakeSlice(targetType, 0, 0)
 		//fmt.Println("targetTypeSlice:", reflect.TypeOf(targetTypeSlice))             // reflect.Value
 		//fmt.Println("targetTypeSlice:", reflect.TypeOf(targetTypeSlice.Interface())) // []*bean.FruitB
-		values := make([]reflect.Value, 0)
-		fmt.Println("values:", reflect.TypeOf(values)) // []reflect.Value
+		destValueSlice := make([]reflect.Value, 0)
+		fmt.Println(reflect.TypeOf(destValueSlice)) // []reflect.Value
 		for i := 0; i < sourceValue.Len(); i++ {
-			sour := sourceValue.Index(i)
-			fmt.Println(sour)
-			fmt.Println(sour.Interface())
-
-			// value := reflect.New(targetType.Elem().Elem())
-			value := reflect.New(targetValue.Type().Elem().Elem())
-			//targetValue.Set(reflect.New(targetValue.Type().Elem()))
-
-			fmt.Println(value)
-			fmt.Println(value.Interface())
-
-			Copy(sour.Interface(), value.Interface())
-
-			fmt.Println(reflect.TypeOf(sour.Interface()))   // *bean.FruitA
-			fmt.Println(reflect.ValueOf(sour.Interface()))  // &{1 名称1 81 0.5372820936538049 2022-05-14 15:20:45.6038055 +0800 CST m=+0.003174001}
-			fmt.Println(reflect.TypeOf(value.Interface()))  // *bean.FruitB
-			fmt.Println(reflect.ValueOf(value.Interface())) // &{1 名称1 81 0.5372820936538049 2022-05-14 15:19:40.1222062 +0800 CST m=+0.002609001 0001-01-01 00:00:00 +0000 UTC }
-
-			values = append(values, value)
-			//values = append(values, reflect.ValueOf(value.Interface()))
-			//targetTypeSlice = append(targetTypeSlice, reflect.ValueOf(value.Interface()))
-
-			//ptr := reflect.New(sliceOfT)
-			//ptr.Elem().Set(reflect.MakeSlice(sliceOfT, 0, 0))
-			//s := ptr.Interface()
-
-			//typeOfT := reflect.TypeOf(t)
-			//sliceOfT := reflect.SliceOf(typeOfT)
-			//s := reflect.MakeSlice(sliceOfT, 0, 0).Interface()
-
-			//e0 = append(e0, reflect.ValueOf(100))
-			//e0 = append(e0, reflect.ValueOf(200))
-			//e0 = append(e0, reflect.ValueOf(300))
-			//e0 = append(e0, reflect.ValueOf(400))
-
-			//reflect.Append(a0, e0...)
-
-			marshal, _ := json.Marshal(value.Interface())
-			fmt.Println(string(marshal))
-		}
-
-		fmt.Println("1111111111111111111")
-		fmt.Println(values[0])
-
-		c := reflect.Append(targetValue, values...)
-
-		//fmt.Printf("变量的地址: %x\n", c)
-		//fmt.Printf("变量的地址: %X\n", dest)
-		//fmt.Printf("变量的地址: %X\n", targetValue)
-
-		fmt.Println(targetValue.CanSet())
-		targetValue.Set(c)
-
-		////targetValue = reflect.Append(targetValue, values...)
-		//
-		//fmt.Println(targetValue)
-		//
-		////targetValue.Set(c)
-		//
-		////SetUnexportedField(targetValue, values)
-		//
-		//fmt.Printf("变量的地址: %x\n", c)
-		//fmt.Printf("变量的地址: %x\n", dest)
-		//
-		//fmt.Println(reflect.TypeOf(c))
-		//fmt.Println(reflect.ValueOf(c))
-		//
-		//fmt.Println(reflect.TypeOf(targetValue))
-		//fmt.Println(reflect.ValueOf(targetValue))
-		//
-		//fmt.Println(c.Kind())
-		//fmt.Println(c.IsNil())
-		//fmt.Println(c.CanAddr())
-		//fmt.Println(c.IsValid())
-		//
-		//fmt.Println(targetValue.Kind())
-		//fmt.Println(targetValue.IsNil())
-		//fmt.Println(targetValue.CanAddr())
-		//fmt.Println(targetValue.IsValid())
-		//
-		//marshalc, _ := json.Marshal(c.Interface())
-		//fmt.Println(string(marshalc))
-		//
-		//marshalb, _ := json.Marshal(targetValue.Interface())
-		//fmt.Println(string(marshalb))
-		//
-		//fmt.Println(c.IsNil())
-		//fmt.Println(targetValue.IsNil())
-		//
-		//l := []int{1, 2}
-		//typeOf := reflect.TypeOf(l)
-		//fmt.Println(typeOf)
-		//fmt.Println(typeOf.Name())
-		//
-		////SetUnexportedField(targetTypeSlice, values)
-		////targetValue.Set(c)
-		////targetTypeSlice.Set(c)
-		//
-		////targetTypeSlice2 := reflect.MakeSlice(targetType, 0, 0)
-		////targetTypeSlice2.Set(c)
-		//
-		//fmt.Println(c)
-		//
-		//SetUnexportedField(targetTypeSlice, c)
-
-		//fmt.Println(targetValue)
-
-		//fmt.Println(targetValue[0])
-		//
-		//
-		//fmt.Println("222222222222222222222")
-		//targetValue.Set(cc)
-
-	case reflect.Ptr:
-		Copy2(sourceType.Elem(), targetValue)
-	case reflect.Struct:
-		for i := 0; i < sourceValue.NumField(); i++ {
-			// 在要修改的结构体中查询有数据结构体中相同属性的字段，有则修改其值
-			name := sourceType.Field(i).Name
-			if ok := targetValue.FieldByName(name).IsValid(); ok {
-				targetValue.FieldByName(name).Set(reflect.ValueOf(sourceValue.Field(i).Interface()))
+			sourceItemValue := sourceValue.Index(i)
+			//if sourceItemValue.Kind() != reflect.Ptr {
+			//	sourceItemValue = sourceItemValue.Addr()
+			//}
+			destItemValue := reflect.New(destItemType)
+			copyObj(sourceItemValue.Interface(), destItemValue.Interface())
+			if ptrLevel == 0 {
+				destItemValue = destItemValue.Elem()
 			}
+			destValueSlice = append(destValueSlice, destItemValue)
+
+			//fmt.Println(sourceItemValue)                              // &{1 名称1 81 0.5372820936538049 2022-05-14 23:08:55.776294 +0  800 CST m=+0.017055401}
+			//fmt.Println(sourceItemValue.Interface())                  // &{1 名称1 81 0.5372820936538049 2022-05-14 23:08:55.776294 +080  0 CST m=+0.017055401}
+			//fmt.Println(destItemValue)                                // &{1 名称1 81 0.5372820936538049 2022-05-14 23:08:55.776294 +0  800 CST m=+0.017055401 0001-01-01 00:00:00 +0000 UTC }
+			//fmt.Println(destItemValue.Interface())                    // &{1 名称1 81 0.5372820936538049 2022-05-14 23:08:55.776294 +0  800 CST m=+0.017055401 0001-01-01 00:00:00 +0000 UTC }
+			//fmt.Println(destItemValue.Elem())                         // {1 名称1 81 0.5372820936538049 2022-05-14 23:17:06.4145388 +0800 C  ST m=+0.017129201 0001-01-01 00:00:00 +0000 UTC }
+			//fmt.Println(destItemValue.Elem().Interface())             // {1 名称1 81 0.5372820936538049 2022-05-14 23:17:06.4145388 +0800 C  ST m=+0.017129201 0001-01-01 00:00:00 +0000 UTC }
+			//fmt.Println(reflect.TypeOf(sourceItemValue.Interface()))  // *bean.FruitA
+			//fmt.Println(reflect.ValueOf(sourceItemValue.Interface())) // &{1 名称1 81 0.5372820936538049 2022-05-14 15:20:45.6038055 +0800 CST m=+0.003174001}
+			//fmt.Println(reflect.TypeOf(destItemValue.Interface()))    // *bean.FruitB
+			//fmt.Println(reflect.ValueOf(destItemValue.Interface()))   // &{1 名称1 81 0.5372820936538049 2022-05-14 15:19:40.1222062 +0800 CST m=+0.002609001 0001-01-01 00:00:00 +0000 UTC }
+			//marshal, _ := json.Marshal(destItemValue.Interface())
+			//fmt.Println(string(marshal))
+		}
+		destValueTemp := reflect.Append(destValue, destValueSlice...)
+		destValue.Set(destValueTemp)
+	case reflect.Struct:
+		copyObj(source, dest)
+	}
+	return errors.New("source type invalid")
+}
+
+// copyObj 将数据源复制到目标对象
+// @source 数据源对象
+// @dest 目标对象
+func copyObj(source, dest interface{}) error {
+	sourceValue := reflect.ValueOf(source)
+	destValue := reflect.ValueOf(dest)
+	if destValue.Kind() != reflect.Ptr {
+		return errors.New("dest value can't a pointer type")
+	}
+	for sourceValue.Kind() == reflect.Ptr {
+		sourceValue = sourceValue.Elem()
+	}
+	if !sourceValue.IsValid() {
+		return errors.New("source value invalid")
+	}
+	for destValue.Kind() == reflect.Ptr {
+		destValue = destValue.Elem()
+	}
+	// 设置结构体中相同属性的值
+	for i := 0; i < sourceValue.NumField(); i++ {
+		fieldName := sourceValue.Type().Field(i).Name
+		if ok := destValue.FieldByName(fieldName).IsValid(); ok {
+			destValue.FieldByName(fieldName).Set(reflect.ValueOf(sourceValue.Field(i).Interface()))
 		}
 	}
-
-	////获取reflect.Type类型
-	//targetValue := reflect.ValueOf(dest).Elem()
-	//sourceValue := reflect.ValueOf(source).Elem()
-	////sourceType := sourceValue.Type()
-	//for i := 0; i < sourceValue.NumField(); i++ {
-	//	// 在要修改的结构体中查询有数据结构体中相同属性的字段，有则修改其值
-	//	name := sourceType.Field(i).Name
-	//	if ok := targetValue.FieldByName(name).IsValid(); ok {
-	//		targetValue.FieldByName(name).Set(reflect.ValueOf(sourceValue.Field(i).Interface()))
-	//	}
-	//}
 	return nil
 }
 
@@ -215,19 +120,3 @@ func GetUnexportedField(field reflect.Value) interface{} {
 func SetUnexportedField(field reflect.Value, value interface{}) {
 	reflect.NewAt(field.Type(), unsafe.Pointer(field.UnsafeAddr())).Elem().Set(reflect.ValueOf(value))
 }
-
-//func CopyObj(source, target interface{}) {
-//	//获取reflect.Type类型
-//	//sourceType := reflect.TypeOf(source)
-//	sourceType := reflect.TypeOf(source)
-//	sourceValue := reflect.ValueOf(source)
-//	targetValue := reflect.ValueOf(target)
-//	//sourceType := sourceValue.Type()
-//	for i := 0; i < sourceValue.NumField(); i++ {
-//		// 在要修改的结构体中查询有数据结构体中相同属性的字段，有则修改其值
-//		name := sourceType.Field(i).Name
-//		if ok := targetValue.FieldByName(name).IsValid(); ok {
-//			targetValue.FieldByName(name).Set(reflect.ValueOf(sourceValue.Field(i).Interface()))
-//		}
-//	}
-//}
--- bean/bean_test.go
+++ bean/bean_test.go
@@ -3,21 +3,90 @@ package bean
 import (
 	"fmt"
 	"math/rand"
-	"reflect"
 	"strconv"
 	"testing"
 	"time"
 )
 
 func TestCopy(t *testing.T) {
-	fruits := getFruits()
-	var result []*FruitB
-	for _, v := range fruits {
-		var item FruitB
-		Copy(&item, v)
-		result = append(result, &item)
+	a := FruitA{
+		ID:         100,
+		Name:       "名称100",
+		Age:        rand.Intn(100),
+		Price:      rand.ExpFloat64(),
+		CreateTime: time.Now(),
 	}
-	fmt.Println(result)
+	//var b = new(FruitB)
+	var b = FruitB{}
+	copyObj(a, b)
+	fmt.Println(b)
+
+	b = FruitB{}
+	copyObj(&a, b)
+	fmt.Println(b)
+
+	b = FruitB{}
+	copyObj(a, &b)
+	fmt.Println(b)
+
+	b = FruitB{}
+	copyObj(&a, &b)
+	fmt.Println(b)
+
+}
+
+func TestCopySlice(t *testing.T) {
+	a1 := getFruitsA(10)
+	a2 := getFruitsPointerA(10)
+
+	var b1 []FruitB
+	Copy(a1, &b1)
+	fmt.Println("b1", b1)
+
+	var b2 []*FruitB
+	Copy(a1, b2)
+	fmt.Println("b2", b2)
+
+	var b3 = new([]FruitB)
+	Copy(a1, &b3)
+	fmt.Println("b3", b3)
+
+	var b4 []FruitB
+	Copy(a2, &b4)
+	fmt.Println("b4", b4)
+
+	var b5 []*FruitB
+	Copy(a2, b5)
+	fmt.Println("b5", b5)
+
+	var b6 = make([]FruitB, 0, 0)
+	Copy(a2, &b6)
+	fmt.Println("b6", b6)
+
+}
+
+/**
+300万条切片中值与地址传递的耗时对比
+[]FruitsA
+12.29s
+12.46s
+12.18s
+11.64s
+
+[]*FruitsA
+12.11s
+12.88s
+12.67s
+11.92s
+*/
+func TestCopySliceTime(t *testing.T) {
+	//a := getFruitsPointerA(3000000)
+	//var b []*FruitB
+	a := getFruitsA(3000000)
+	var b []FruitB
+	err := Copy(a, &b)
+	fmt.Println(err)
+	fmt.Println(len(b))
 }
 
 type FruitA struct {
@@ -38,9 +107,9 @@ type FruitB struct {
 	Xss        string
 }
 
-func getFruits() []*FruitA {
+func getFruitsPointerA(len int) []*FruitA {
 	var data []*FruitA
-	for i := 0; i < 10; i++ {
+	for i := 0; i < len; i++ {
 		data = append(data, &FruitA{
 			ID:         uint(i + 1),
 			Name:       "名称" + strconv.Itoa(i+1),
@@ -52,165 +121,16 @@ func getFruits() []*FruitA {
 	return data
 }
 
-func TestCopyList(t *testing.T) {
-
-	//var x float64 = 3.4
-	//v := reflect.ValueOf(&x)
-	//tx := reflect.TypeOf(&x)
-	//fmt.Println(tx)                // *float64
-	//fmt.Println(v.Elem().CanSet()) // true
-
-	//var x = new(FruitB)
-	//v := reflect.ValueOf(&x)
-	//tx := reflect.TypeOf(&x)
-	//fmt.Println(tx)                       // **bean.FruitB
-	//fmt.Println(v.CanSet())               // false
-	//fmt.Println(v.Elem().CanSet())        // true
-	//fmt.Println(v.Elem().Elem().CanSet()) // true
-
-	//x := FruitB{}
-	//v := reflect.ValueOf(x)
-	//tx := reflect.TypeOf(x)
-	//fmt.Println(tx)         // bean.FruitB
-	//fmt.Println(v.CanSet()) // false
-
-	//var x []*FruitB
-	//v := reflect.ValueOf(&x)
-	//tx := reflect.TypeOf(&x)
-	//fmt.Println(tx)                // *[]*bean.FruitB
-	//fmt.Println(v.Elem().CanSet()) // true
-
-	var x []*FruitB
-	tx := reflect.TypeOf(x) // []*bean.FruitB
-	v := reflect.ValueOf(x) // []
-	fmt.Println(tx)         // []*bean.FruitB
-	fmt.Println(v)          // []
-	fmt.Println(v.CanSet()) // false
-
-	//var x []*FruitB
-	//v := reflect.ValueOf(x) // []
-	//tx := reflect.TypeOf(x) // []*bean.FruitB
-	//fmt.Println(tx)         // []*bean.FruitB
-	//fmt.Println(v)          // []
-	//fmt.Println(v.CanSet()) // false
-	//fmt.Println(v.Elem().CanSet()) // false
-
-	fruits := getFruits()
-	var source []*FruitA
-	for _, v := range fruits {
-		source = append(source, v)
+func getFruitsA(len int) []FruitA {
+	var data []FruitA
+	for i := 0; i < len; i++ {
+		data = append(data, FruitA{
+			ID:         uint(i + 101),
+			Name:       "名称" + strconv.Itoa(i+1),
+			Age:        rand.Intn(100),
+			Price:      rand.ExpFloat64(),
+			CreateTime: time.Now(),
+		})
 	}
-	var source2 []*FruitB
-
-	Copy2(&source, &source2)
-	Copy2(source, &source2)
-
-	//var source2 *FruitA
-	//sourceType3 := reflect.TypeOf(source2)
-	//fmt.Println(sourceType3.Kind())
-
-	//var source2 [5]*FruitA
-	//sourceType3 := reflect.TypeOf(source2)
-	//fmt.Println(sourceType3.Elem().Elem().Kind())
-
-	sourceType := reflect.TypeOf(source)
-	fmt.Println(sourceType)                             // []*bean.FruitA
-	fmt.Println(sourceType.Kind())                      // slice
-	fmt.Println(sourceType.Elem())                      // *bean.FruitA
-	fmt.Println(sourceType.Elem().Kind())               // ptr
-	fmt.Println(sourceType.Elem().Elem())               // bean.FruitA
-	fmt.Println(sourceType.Elem().Elem().Kind())        // struct
-	fmt.Println(sourceType.Elem().Elem().Field(0).Name) // ID
-
-	i1 := &FruitA{}
-	i2 := &i1
-	fmt.Println(reflect.ValueOf(i2))               // 0xc000006048
-	fmt.Println(reflect.ValueOf(i2).Elem())        // &{0  0 0 0001-01-01 00:00:00 +0000 UTC}
-	fmt.Println(reflect.ValueOf(i2).Elem().Elem()) // {0  0 0 0001-01-01 00:00:00 +0000 UTC}
-
-	//fmt.Println(sourceType.Elem().Field(0).Name)	//
-
-	sourceValue := reflect.ValueOf(source[0]).Elem()
-	sourceType2 := sourceValue.Type()
-	fmt.Println(sourceValue)               // {1 名称1 81 0.5372820936538049 2022-05-13 10:46:15.6915219 +0800 CST m=+0.002175701}
-	fmt.Println(sourceType2)               // bean.FruitA
-	fmt.Println(sourceType2.Field(0).Name) // ID
-
-	//CopyList(source, &bs)
-
-	////先获取reflect.Type
-	//rTyp := reflect.TypeOf(source)
-	//fmt.Println(rTyp) // []*bean.FruitA
-
-	//sliceType := reflect.SliceOf(rTyp)
-	//fmt.Println(sliceType)
-	//
-	////2.获取到reflect.value
-	//rVal := reflect.ValueOf(source)
-	//fmt.Println("rVal = ", rVal)
-	////fmt.Println("rVal.NumField():", rVal.NumField())
-	////fmt.Println("rVal.NumField():", len(rVal))
-	//
-	//sourceValue := reflect.ValueOf(source[0]).Elem()
-	//fmt.Println("sourceValue:", sourceValue)
-	//fmt.Println("sourceValue.NumField():", sourceValue.NumField())
-
-	//out := sourceType1.In(0)
-	//out := sourceType1.Out(0)
-	//fmt.Println(out)
-
-	//sliceType := reflect.SliceOf(sourceType1)
-	//fmt.Println(sliceType)
-
-	//of := reflect.ArrayOf(0, sourceType1)
-	//fmt.Println(of)
-	//
-	//fmt.Println(sourceType1)
-	//fmt.Println(sourceType1.Field(0).Name)
-
-	//sourceValue := reflect.ValueOf(source[0]).Elem()
-	//sourceType := sourceValue.Type()
-	//fmt.Println(sourceType)
-	//fmt.Println(sourceType.Field(0).Name)
-
-	// begin
-
-	// END
-
-	var list []*FruitA
-	if reflect.TypeOf(source).Kind() == reflect.Slice {
-		s := reflect.ValueOf(source)
-		for i := 0; i < s.Len(); i++ {
-			ele := s.Index(i)
-			fmt.Println(ele)
-			fmt.Println(ele.Interface())
-			list = append(list, ele.Interface().(*FruitA))
-		}
-	}
-
-	if sourceType.Kind() == reflect.Slice {
-		var list []*FruitA
-		if reflect.TypeOf(source).Kind() == reflect.Slice {
-			s := reflect.ValueOf(source)
-			for i := 0; i < s.Len(); i++ {
-				ele := s.Index(i)
-				fmt.Println(ele)
-				fmt.Println(ele.Interface())
-				list = append(list, ele.Interface().(*FruitA))
-			}
-		}
-
-		sliceType := reflect.SliceOf(reflect.TypeOf(source))
-		slice := reflect.MakeSlice(sliceType, 0, 0)
-		slicedata := reflect.New(slice.Type())
-		slicedata.Elem().Set(slice)
-		data := slicedata.Interface()
-		fmt.Println(data)
-		fmt.Println(slicedata)
-
-		//for i := 0; i < sourceValue.NumField(); i++ {
-		//
-		//}
-	}
-
+	return data
 }
