9b1448c6e0eed2b926670b01c96cd04fcd707580
sent <3288182@qq.com>
2022-10-05 23:45:22 +0800

    add times util

---
+++ times/times.go
@@ -0,0 +1,70 @@
+package times
+
+import (
+	"strconv"
+	"time"
+)
+
+// DateString 获取日期字符串
+func DateString(t time.Time) string {
+	return t.Format("20060102")
+}
+
+// TimeString 获取时间字符串
+func TimeString(t time.Time) string {
+	return t.Format("15:04:05")
+}
+
+// AddDuration 增加时间
+func AddDuration(t time.Time, interval int, unit string) time.Time {
+	duration, err := time.ParseDuration(strconv.Itoa(interval) + unit)
+	if err != nil {
+		panic(err)
+	}
+	return t.Add(duration)
+}
+
+// AddSecond add seconds
+func AddSecond(t time.Time, seconds int) time.Time {
+	return AddDuration(t, seconds, "s")
+}
+
+// AddMinute add minutes
+func AddMinute(t time.Time, minutes int) time.Time {
+	return AddDuration(t, minutes, "m")
+}
+
+// AddHour add hours
+func AddHour(t time.Time, hours int) time.Time {
+	return AddDuration(t, hours, "h")
+}
+
+// AddDay add days
+func AddDay(t time.Time, days int) time.Time {
+	return t.AddDate(0, 0, days)
+}
+
+// AddMonth add months
+func AddMonth(t time.Time, months int) time.Time {
+	return t.AddDate(0, months, 0)
+}
+
+// AddYear add years
+func AddYear(t time.Time, years int) time.Time {
+	return t.AddDate(years, 0, 0)
+}
+
+// StartDateTime 当天的开始时间
+func StartDateTime(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.Local)
+}
+
+// EndDateTime 当天的结束时间
+func EndDateTime(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 23, 59, 59, 999999999, time.Local)
+}
+
+// RangeDateTime 当天的开始与结束时间
+func RangeDateTime(t time.Time) (time.Time, time.Time) {
+	return StartDateTime(t), EndDateTime(t)
+}
---
+++ times/times_test.go
@@ -0,0 +1,25 @@
+package times
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestTodayAddDay(t *testing.T) {
+	now := time.Now()
+	fmt.Println(now)             // 2022-10-05 23:35:22.3942125 +0800 CST m=+0.003905101
+	fmt.Println(DateString(now)) // 20221005
+	fmt.Println(TimeString(now)) // 23:35:22
+	// 增加1小时
+	fmt.Println(AddDuration(now, 1, "h")) // 2022-10-06 00:35:22.3942125 +0800 CST m=+3600.003905101
+	fmt.Println(AddSecond(now, 1))        // 2022-10-05 23:35:23.3942125 +0800 CST m=+1.003905101
+	fmt.Println(AddMinute(now, 1))        // 2022-10-05 23:36:22.3942125 +0800 CST m=+60.003905101
+	fmt.Println(AddHour(now, 1))          // 2022-10-06 00:35:22.3942125 +0800 CST m=+3600.003905101
+	fmt.Println(AddDay(now, 1))           // 2022-10-06 23:35:22.3942125 +0800 CST
+	fmt.Println(AddMonth(now, 1))         // 2022-11-05 23:35:22.3942125 +0800 CST
+	fmt.Println(AddYear(now, 1))          // 2023-10-05 23:35:22.3942125 +0800 CST
+	fmt.Println(StartDateTime(now))       // 2022-10-05 00:00:00 +0800 CST
+	fmt.Println(EndDateTime(now))         // 2022-10-05 23:59:59.999999999 +0800 CST
+	fmt.Println(RangeDateTime(now))       // 2022-10-05 00:00:00 +0800 CST  2022-10-05 23:59:59.999999999 +0800 CST
+}
