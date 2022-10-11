248feb1c7691d7f1bc3ce93eb3b95eb2343aea98
sent <3288182@qq.com>
2022-10-12 00:31:05 +0800

    rename json to jsonx

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/minlib/go-util
 go 1.19
 
 require (
-	github.com/google/uuid v1.3.0 // indirect
-	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be // indirect
-	golang.org/x/exp v0.0.0-20221004215720-b9f4876ce741 // indirect
+	github.com/google/uuid v1.3.0
+	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be
+	golang.org/x/exp v0.0.0-20221004215720-b9f4876ce741
 )
--- jsonx/jsonx.go
+++ jsonx/jsonx.go
@@ -1,12 +1,12 @@
-package json
+package jsonx
 
 import (
 	"encoding/json"
 	"errors"
 )
 
-// ToJsonString 对象转为Json字符串
-func ToJsonString(v any) string {
+// Marshal returns the JSON encoding string of v.
+func Marshal(v any) string {
 	if buf, err := json.Marshal(v); err == nil {
 		return string(buf)
 	} else {
@@ -14,7 +14,7 @@ func ToJsonString(v any) string {
 	}
 }
 
-// Parse Json字符串转为对象
-func Parse(data string, v any) error {
+// Unmarshal parses the JSON-encoded data and stores the result.
+func Unmarshal(data string, v any) error {
 	return json.Unmarshal([]byte(data), v)
 }
