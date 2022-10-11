b6431c20df15c9df8fa24e1a287fb4dd6804be38
sent <3288182@qq.com>
2022-10-12 00:22:28 +0800

    add mapx util

---
+++ mapx/mapx.go
@@ -0,0 +1,7 @@
+package mapx
+
+// ContainsKey returns the keys of the map m contains a key k.
+func ContainsKey[M ~map[K]V, K comparable, V any](m M, k K) bool {
+	_, found := m[k]
+	return found
+}
---
+++ mapx/mapx_test.go
@@ -0,0 +1,18 @@
+package mapx
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestContainsKey(t *testing.T) {
+	map1 := make(map[string]interface{})
+	map1["a"] = 1
+	fmt.Println(ContainsKey(map1, "a")) // true
+	fmt.Println(ContainsKey(map1, "b")) // false
+
+	map2 := make(map[int]bool)
+	map2[1] = true
+	fmt.Println(ContainsKey(map2, 1)) // true
+	fmt.Println(ContainsKey(map2, 2)) // false
+}
