e701ebf52943249f947764a1db1f1f0c08a8fcfc
sent <3288182@qq.com>
2023-01-07 18:34:45 +0800

    convert

--- convert/convert.go
+++ convert/convert.go
@@ -3,6 +3,7 @@ package convert
 import (
 	"bytes"
 	"encoding/binary"
+	"strconv"
 
 	"golang.org/x/exp/constraints"
 )
@@ -18,4 +19,22 @@ func BytesToInt[E constraints.Integer](b []byte) E {
 	var res int64
 	binary.Read(buf, binary.BigEndian, &res)
 	return E(res)
+}
+
+func IntToString[E constraints.Integer](i E) string {
+	return strconv.FormatInt(int64(i), 10)
+}
+
+func StringToInt[E constraints.Integer](s string) (E, error) {
+	i, err := strconv.ParseInt(s, 10, 64)
+	return E(i), err
+}
+
+func FloatToString[E constraints.Float](f E, prec int) string {
+	return strconv.FormatFloat(float64(f), 'f', prec, 64)
+}
+
+func StringToFloat[E constraints.Float](s string) (E, error) {
+	f, err := strconv.ParseFloat(s, 64)
+	return E(f), err
 }
--- convert/convert_test.go
+++ convert/convert_test.go
@@ -10,7 +10,7 @@ func TestIntToBytes(t *testing.T) {
 	fmt.Printf("%T, %v\n", b, b) // []uint8, [255 255 255 255 255 255 255 145]
 }
 
-func TestBytesToInt2(t *testing.T) {
+func TestBytesToInt(t *testing.T) {
 	b := IntToBytes(-111)
 	i := BytesToInt[int](b)
 	want := -111
@@ -20,4 +20,44 @@ func TestBytesToInt2(t *testing.T) {
 	}
 	fmt.Printf("%T, %v\n", i, i)     // int, -111
 	fmt.Printf("%T, %v\n", i64, i64) // int64, -111
+}
+
+func TestIntToString(t *testing.T) {
+	print(IntToString(int(100)))
+	print(IntToString(int8(8)))
+	print(IntToString(int32(16)))
+	print(IntToString(int32(32)))
+	print(IntToString(int64(64)))
+}
+
+func TestStringToInt(t *testing.T) {
+	print2(StringToInt[int]("100"))
+	print2(StringToInt[int8]("100"))
+	print2(StringToInt[int16]("100"))
+	print2(StringToInt[int32]("100"))
+	print2(StringToInt[int64]("100"))
+}
+
+func TestFloatToString(t *testing.T) {
+	print(FloatToString(float32(100.1111), -1))
+	print(FloatToString(float32(100.1111), 2))
+	print(FloatToString(float32(100.9999), 2))
+	print(FloatToString(float64(100.1111), -1))
+	print(FloatToString(float64(100.1111), 2))
+	print(FloatToString(float64(100.9999), 2))
+}
+
+func TestStringToFloat(t *testing.T) {
+	print2(StringToFloat[float32]("100.1111"))
+	print2(StringToFloat[float32]("100.9999"))
+	print2(StringToFloat[float64]("100.1111"))
+	print2(StringToFloat[float64]("100.9999"))
+}
+
+func print(i any) {
+	fmt.Printf("%T, %v\n", i, i)
+}
+
+func print2(s any, err error) {
+	fmt.Printf("%T, %v, err: %v\n", s, s, err)
 }
