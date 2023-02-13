b851a2382f81d07dd277b9bf68bbc818d3512a1b
shentao <3288182@qq.com>
2023-02-14 00:03:03 +0800

    add json_datetime and  json_long_slice

---
+++ json/json_datetime.go
@@ -0,0 +1,62 @@
+package json
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"time"
+)
+
+const (
+	formatDateTime = "2006-01-02 15:04:05"
+)
+
+type DateTime struct {
+	time.Time
+}
+
+func NewDateTime(t time.Time) *DateTime {
+	return &DateTime{t}
+}
+
+// Scan implements the Scanner interface.
+func (t *DateTime) Scan(v interface{}) error {
+	if value, ok := v.(time.Time); ok {
+		*t = DateTime{Time: value}
+		return nil
+	} else {
+		return fmt.Errorf("can not convert %v to timestamp", v)
+	}
+}
+
+// Value insert timestamp into mysql need this function.
+func (t DateTime) Value() (driver.Value, error) {
+	var zeroTime time.Time
+	if t.Time.Unix() == zeroTime.Unix() {
+		return nil, nil
+	} else {
+		return t.Time, nil
+	}
+}
+
+// MarshalJSON implements the json.Marshaler interface.
+// MarshalJSON on DateTime format Time field with %Y-%m-%d %H:%M:%S
+func (t DateTime) MarshalJSON() ([]byte, error) {
+	var zeroTime time.Time
+	if t.Time.Unix() == zeroTime.Unix() {
+		return []byte("null"), nil
+	} else {
+		formatted := fmt.Sprintf("\"%s\"", t.Format(formatDateTime))
+		return []byte(formatted), nil
+	}
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+func (t *DateTime) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" {
+		return nil
+	}
+	// Fractional seconds are handled implicitly by Parse.
+	var err error
+	(*t).Time, err = time.ParseInLocation(`"`+formatDateTime+`"`, string(data), time.Local)
+	return err
+}
---
+++ json/json_long_slice_test.go
@@ -0,0 +1,12 @@
+package json
+
+// func TestInt64StringSlice_MarshalJSON(t *testing.T) {
+// 	slice := LongSlice{123, 34}
+// 	marshal, _ := json.Marshal(slice)
+// 	fmt.Println(string(marshal))
+
+// 	jsonString := "[\"11111111\",\"2222222222222\"]"
+// 	var result LongSlice
+// 	json.Unmarshal([]byte(jsonString), &result)
+// 	fmt.Println(result)
+// }
