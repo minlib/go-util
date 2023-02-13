e5920fafbdd9f224b9668db2fd9accc0918e62ae
shentao <3288182@qq.com>
2023-02-14 00:10:09 +0800

    rename package name "jsonx" to "json"

--- errorx/error_test.go
+++ errorx/error_test.go
@@ -4,18 +4,18 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/minlib/go-util/jsonx"
+	"github.com/minlib/go-util/json"
 )
 
 func TestNew(t *testing.T) {
 	var err1 *Error = New(500, "server busy")
 	fmt.Println(err1)
-	fmt.Println(jsonx.Marshal(err1))
+	fmt.Println(json.MarshalString(err1))
 	fmt.Println(err1.Code)
 	fmt.Println(err1.Message)
 	fmt.Println(err1.Error())
 
 	var err2 error = New(500, "server busy")
 	fmt.Println(err2)
-	fmt.Println(jsonx.Marshal(err2))
+	fmt.Println(json.MarshalString(err2))
 }
