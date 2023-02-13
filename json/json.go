22329b3c63a3216960e25676568f2cef70cd2bf0
shentao <3288182@qq.com>
2023-02-14 00:03:18 +0800

    optimize json code

--- json/json.go
+++ json/json.go
@@ -2,19 +2,28 @@ package json
 
 import (
 	"encoding/json"
-	"errors"
 )
 
 // Marshal returns the JSON encoding string of v.
-func Marshal(v any) string {
-	if buf, err := json.Marshal(v); err == nil {
-		return string(buf)
-	} else {
-		panic(errors.New("json serialization error"))
-	}
+func Marshal(v any) ([]byte, error) {
+	return json.Marshal(v)
 }
 
 // Unmarshal parses the JSON-encoded data and stores the result.
-func Unmarshal(data string, v any) error {
+func Unmarshal(data []byte, v any) error {
+	return json.Unmarshal(data, v)
+}
+
+// MarshalString returns the JSON encoding string of v.
+func MarshalString(v any) string {
+	if buf, err := json.Marshal(v); err == nil {
+		return string(buf)
+	} else {
+		return ""
+	}
+}
+
+// UnmarshalString parses the JSON-encoded data and stores the result.
+func UnmarshalString(data string, v any) error {
 	return json.Unmarshal([]byte(data), v)
 }
--- json/json_test.go
+++ json/json_test.go
@@ -5,26 +5,55 @@ import (
 	"testing"
 )
 
-func TestMarshal(t *testing.T) {
-	fmt.Println(Marshal([]int64{12, 34, 567}))       // [12,34,567]
-	fmt.Println(Marshal([]string{"A", "BC", "DEF"})) // ["A","BC","DEF"]
+// import (
+// 	"fmt"
+// 	"testing"
+// )
+
+// func TestMarshal(t *testing.T) {
+// 	fmt.Println(Marshal([]int64{12, 34, 567}))       // [12,34,567]
+// 	fmt.Println(Marshal([]string{"A", "BC", "DEF"})) // ["A","BC","DEF"]
+
+// 	m := make(map[string]any, 2)
+// 	m["age"] = 24
+// 	m["name"] = "张三"
+// 	fmt.Println(Marshal(m)) // {"age":24,"name":"张三"}
+// }
+
+// func TestUnmarshal(t *testing.T) {
+// 	i := []int64{}
+// 	Unmarshal("[12,34,567]", &i)
+// 	fmt.Println(i) // [12 34 567]
+
+// 	s := []string{}
+// 	Unmarshal("[\"A\",\"BC\",\"DEF\"]", &s)
+// 	fmt.Println(s) // [A BC DEF]
+
+// 	m := make(map[string]any, 0)
+// 	Unmarshal("{\"age\":24,\"name\":\"张三\"}", &m)
+// 	fmt.Println(m) // map[age:24 name:张三]
+// }
+
+func TestMarshalString(t *testing.T) {
+	fmt.Println(MarshalString([]int64{12, 34, 567}))       // [12,34,567]
+	fmt.Println(MarshalString([]string{"A", "BC", "DEF"})) // ["A","BC","DEF"]
 
 	m := make(map[string]any, 2)
 	m["age"] = 24
 	m["name"] = "张三"
-	fmt.Println(Marshal(m)) // {"age":24,"name":"张三"}
+	fmt.Println(MarshalString(m)) // {"age":24,"name":"张三"}
 }
 
-func TestUnmarshal(t *testing.T) {
+func TestUnmarshalString(t *testing.T) {
 	i := []int64{}
-	Unmarshal("[12,34,567]", &i)
+	UnmarshalString("[12,34,567]", &i)
 	fmt.Println(i) // [12 34 567]
 
 	s := []string{}
-	Unmarshal("[\"A\",\"BC\",\"DEF\"]", &s)
+	UnmarshalString("[\"A\",\"BC\",\"DEF\"]", &s)
 	fmt.Println(s) // [A BC DEF]
 
 	m := make(map[string]any, 0)
-	Unmarshal("{\"age\":24,\"name\":\"张三\"}", &m)
+	UnmarshalString("{\"age\":24,\"name\":\"张三\"}", &m)
 	fmt.Println(m) // map[age:24 name:张三]
 }
