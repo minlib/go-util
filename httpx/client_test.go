ad449bbd9656b4ece2d37f9918b5f8ef6a34c1e0
shentao <3288182@qq.com>
2023-05-22 00:31:00 +0800

    optimized http request util

--- httpx/client_test.go
+++ httpx/client_test.go
@@ -15,8 +15,9 @@ func TestGet(t *testing.T) {
 }
 
 func TestPost(t *testing.T) {
-	data := make(map[string]string, 4)
-	data["prompt"] = "这里是内容"
+	data := map[string]string{
+		"prompt": "这里是内容",
+	}
 	if resp, err := Post("http://localhost:8080/test", "application/json", jsonx.MarshalString(data)); err != nil {
 		fmt.Println(err)
 	} else {
