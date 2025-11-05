e244f30ca68a343cde9e142371217fb6398f9829
stao <3288182@qq.com>
2025-11-05 20:53:32 +0800

    Add the function of converting between RGBA and hexadecimal colors

--- colorx/color.go
+++ colorx/color.go
@@ -1,6 +1,7 @@
 package colorx
 
 import (
+	"image/color"
 	"strconv"
 	"strings"
 )
@@ -28,4 +29,43 @@ func Hex2RGB(hex string) (uint8, uint8, uint8) {
 	g, _ := strconv.ParseUint(hex[2:4], 16, 10)
 	b, _ := strconv.ParseUint(hex[4:6], 16, 10)
 	return uint8(r), uint8(g), uint8(b)
+}
+
+// RGBA2Hex 将 color.RGBA 转换为十六进制字符串
+func RGBA2Hex(c color.RGBA) string {
+	return "#" + toHex(c.R) + toHex(c.G) + toHex(c.B) + toHex(c.A)
+}
+
+// Hex2RGBA 将十六进制颜色字符串转换为 color.RGBA
+func Hex2RGBA(hex string) color.RGBA {
+	// 移除 # 前缀
+	hex = strings.TrimPrefix(hex, "#")
+
+	// 处理不同的十六进制格式
+	switch len(hex) {
+	case 3: // RGB 格式
+		r, _ := strconv.ParseUint(string(hex[0])+string(hex[0]), 16, 8)
+		g, _ := strconv.ParseUint(string(hex[1])+string(hex[1]), 16, 8)
+		b, _ := strconv.ParseUint(string(hex[2])+string(hex[2]), 16, 8)
+		return color.RGBA{R: uint8(r), G: uint8(g), B: uint8(b), A: 255}
+	case 4: // RGBA 格式
+		r, _ := strconv.ParseUint(string(hex[0])+string(hex[0]), 16, 8)
+		g, _ := strconv.ParseUint(string(hex[1])+string(hex[1]), 16, 8)
+		b, _ := strconv.ParseUint(string(hex[2])+string(hex[2]), 16, 8)
+		a, _ := strconv.ParseUint(string(hex[3])+string(hex[3]), 16, 8)
+		return color.RGBA{R: uint8(r), G: uint8(g), B: uint8(b), A: uint8(a)}
+	case 6: // RRGGBB 格式
+		r, _ := strconv.ParseUint(hex[0:2], 16, 8)
+		g, _ := strconv.ParseUint(hex[2:4], 16, 8)
+		b, _ := strconv.ParseUint(hex[4:6], 16, 8)
+		return color.RGBA{R: uint8(r), G: uint8(g), B: uint8(b), A: 255}
+	case 8: // RRGGBBAA 格式
+		r, _ := strconv.ParseUint(hex[0:2], 16, 8)
+		g, _ := strconv.ParseUint(hex[2:4], 16, 8)
+		b, _ := strconv.ParseUint(hex[4:6], 16, 8)
+		a, _ := strconv.ParseUint(hex[6:8], 16, 8)
+		return color.RGBA{R: uint8(r), G: uint8(g), B: uint8(b), A: uint8(a)}
+	default:
+		return color.RGBA{R: 0, G: 0, B: 0, A: 255} // 默认黑色
+	}
 }
--- colorx/color_test.go
+++ colorx/color_test.go
@@ -2,6 +2,7 @@ package colorx
 
 import (
 	"fmt"
+	"image/color"
 	"testing"
 )
 
@@ -15,4 +16,22 @@ func TestRGB2Hex(t *testing.T) {
 	fmt.Println(RGB2Hex(11, 33, 243))
 	fmt.Println(RGB2Hex(0, 0, 0))
 	fmt.Println(RGB2Hex(255, 255, 255))
+}
+
+func TestRGBA2Hex(t *testing.T) {
+	rgba := color.RGBA{R: 255, G: 255, B: 255, A: 255}
+	hex := RGBA2Hex(rgba)
+	fmt.Println(hex) // 应该输出 "#ffffffff"
+
+	rgba2 := color.RGBA{R: 11, G: 33, B: 243, A: 128}
+	hex2 := RGBA2Hex(rgba2)
+	fmt.Println(hex2) // 应该输出 "#0b21f380"
+}
+
+func TestHex2RGBA(t *testing.T) {
+	rgba := Hex2RGBA("#ffffffff")
+	fmt.Printf("RGBA: %+v\n", rgba) // 应该输出 {R:255 G:255 B:255 A:255}
+
+	rgba2 := Hex2RGBA("#0b21f380")
+	fmt.Printf("RGBA: %+v\n", rgba2) // 应该输出 {R:11 G:33 B:243 A:128}
 }
